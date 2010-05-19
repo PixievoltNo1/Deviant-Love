d3cb2ef2530a7e7bfc5bb9bcaa752c0bcc9b32f4
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2010-05-18 22:29:45 -0700

    Rewrite + v7 compatibility
    When a hashchange happens, Chrome re-executes the script but doesn't reset its environment. detector.js was rewritten with this and v7 in mind.

--- Chrome/detector.js
+++ Chrome/detector.js
@@ -3,16 +3,23 @@
 	Copyright 2010 Pikadude No. 1
 	Check core.js for the complete legal stuff.
 */
-var feedHref = $('link[rel="alternate"][type="application/rss+xml"]').attr("href");
+if (!baseFeedHref) {
+	var baseFeedHref = $('link[rel="alternate"][type="application/rss+xml"]').attr("href");
+}
 if (location.hash == "" || location.hash.indexOf("#_featured") == 0) {
 	var pageType = "featured";
+	var feedHref = baseFeedHref;
 } else if (location.hash.indexOf("#_browse") == 0) {
 	var pageType = "allFaves";
-	feedHref = feedHref.replace(/(favby%3A.+)%2F[0-9]+(&)/, "$1$2");
+	var feedHref = feedHref.replace(/(favby%3A.+)%2F[0-9]+(&)/, "$1$2");
+} else if (location.hash.charAt(1) == "/") {
+	if (!pageType) { throw "STOP! Hammertime." } // This content script started running on a deviation page, so don't allow the heart to appear.
 } else {
 	var pageType = "collection";
+	var feedHref = baseFeedHref;
 	// TODO: Rewrite feedHref to use the ID number of this collection
 }
+
 // var deviantName = jQuery(".gruserbadge h1 a").text();
 chrome.extension.sendRequest({action: "showLove"});
 chrome.extension.onRequest.addListener( function(thing, buddy, callback) {switch (thing.action) {
