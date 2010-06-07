f5c31a8f1c0272274f0206730731536bf4eb68c8
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2010-06-07 03:41:21 -0700

    detector.js rewritten for Chrome 5 compatibility (and only Chrome 5 compatibility); basic functionality complete

--- Chrome/detector.js
+++ Chrome/detector.js
@@ -3,28 +3,38 @@
 	Copyright 2010 Pikadude No. 1
 	Check core.js for the complete legal stuff.
 */
-if (!baseFeedHref) {
-	var baseFeedHref = document.querySelector('link[rel="alternate"][type="application/rss+xml"]')
-		.getAttribute("href");
-}
-if (location.hash == "" || location.hash.indexOf("#_featured") == 0) {
-	var pageType = "featured";
-	var feedHref = baseFeedHref;
-} else if (location.hash.indexOf("#_browse") == 0) {
-	var pageType = "allFaves";
-	var feedHref = baseFeedHref.replace(/(favby%3A.+)%2F[0-9]+(&)/, "$1$2");
-} else if (location.hash.charAt(1) == "/") {
-	if (!pageType) { throw "STOP! Hammertime." } // This content script started running on a deviation page, so don't allow the heart to appear.
-} else {
-	var pageType = "collection";
-	var feedHref = baseFeedHref;
-	// TODO: Rewrite feedHref to use the ID number of this collection
-}
 
-// var deviantName = document.querySelector(".gruserbadge a.u").innerText;
-chrome.extension.sendRequest({action: "showLove"});
-chrome.extension.onRequest.addListener( function(thing, buddy, callback) {switch (thing.action) {
-	case "getLove":
-		callback({"feedHref": feedHref, "pageType": pageType});
-	break;
-}} );
+var baseFeedHref = document.querySelector('link[rel="alternate"][type="application/rss+xml"]')
+	.getAttribute("href");
+var pageType, feedHref, firstInit = true;
+	
+function findLove() {
+	if (location.hash == "" || location.hash.indexOf("#_featured") == 0) {
+		pageType = "featured";
+		feedHref = baseFeedHref;
+	} else if (location.hash.indexOf("#_browse") == 0) {
+		pageType = "allFaves";
+		feedHref = baseFeedHref.replace(/(favby%3A.+?)%2F[0-9]+(&)/, "$1$2");
+	} else if (location.hash.charAt(1) == "/") {
+		return; // This content script started running on a deviation page, so don't allow the heart to appear.
+	} else {
+		pageType = "collection";
+		var haystack = document.querySelector('.folderview-control').className; // The collection ID is in here somewhere
+		// TODO: The above querySelector may return null if dA changes their layout. Rewrite this to fail gracefully.
+		var collectionID = /gallery-([0-9]+)/.exec(haystack)[1];
+		feedHref = baseFeedHref.replace(/(favby%3A.+?%2F)[0-9]+(&)/, "$1" + collectionID + "$2");
+	}
+	// var deviantName = document.querySelector(".gruserbadge a.u").innerText;
+	
+	chrome.extension.sendRequest({action: "showLove"});
+	if (firstInit) {
+		firstInit = false;
+		chrome.extension.onRequest.addListener( function(thing, buddy, callback) {switch (thing.action) {
+			case "getLove":
+				callback({"feedHref": feedHref, "pageType": pageType});
+			break;
+		}} );
+	}
+}
+findLove();
+addEventListener("hashchange", findLove, false);
