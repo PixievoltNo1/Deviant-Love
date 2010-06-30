3dbadc3f04545afccc8203b015f5e350bd9a860c
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2010-06-30 02:20:13 -0700

    A Firefox folder appeared! :o
    It doesn't do anything at the moment. Theoretically, when installed it should show a heart that can be clicked to display a useless popup, but the popup doesn't appear for me. Will investigate further when I'm not supposed to be asleep.

---
+++ Firefox/content/overlay.js
@@ -0,0 +1,12 @@
+var DeviantLove = {
+  onLoad: function() {
+    // initialization code
+    this.initialized = true;
+  },
+
+  onMenuItemCommand: function(e) {
+  
+  }
+};
+
+window.addEventListener("load", DeviantLove.onLoad, false);
