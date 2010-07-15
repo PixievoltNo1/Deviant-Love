fd1c7d9ca39ab6927f56a46ad0c4b10439e005fe
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2010-07-14 19:34:10 -0700

    After many failed attempts to make the popup work, I'm saying "Screw the popup until Jetpack's Panel API is released".
    Until that point, future work on Deviant Love will have the important part running in its own tab.

--- Firefox/content/overlay.js
+++ Firefox/content/overlay.js
@@ -1,12 +1,10 @@
 var DeviantLove = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-  },
-
-  onMenuItemCommand: function(e) {
-  
-  }
+	onLoad: function() {
+		var heart = document.getElementById("Deviant-Love-Heart");
+		heart.addEventListener("command", function() {
+			
+		}, false);
+	}
 };
 
 window.addEventListener("load", DeviantLove.onLoad, false);
