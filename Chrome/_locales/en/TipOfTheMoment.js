4975a9e9a791f5f75fca366121eb144bea1b255e
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2011-03-14 20:36:46 -0700

    Not very related to the purpose of the devwatchcheck branch, but revising & adding to the Tips anyhow. :D

--- Chrome/_locales/en/TipOfTheMoment.js
+++ Chrome/_locales/en/TipOfTheMoment.js
@@ -1,7 +1,9 @@
 var tips = [
 {"icon": "Random.png", "html": "Love a <a href='http://www.deviantart.com/random/deviant'>random deviant</a> (but don't be creepy)."},
-{"icon": "SilkIcons/heart.png", "html": "Love life. Join <a href='http://deviantheart.deviantart.com/'>#deviantHEART</a>."},
+{"icon": "SilkIcons/heart.png", "html": "Love everything. Join <a href='http://deviantheart.deviantart.com/'>#deviantHEART</a>."},
 {"icon": "Pikadude.png", "html": "You can show your love for Deviant Love at <a href='http://fav.me/d2my13o'>its deviation page</a>."},
-{"icon": "Fella.png", "html": "There's lots of love-worthy art to be found at <a href='http://today.deviantart.com/dds/'>Daily Deviations</a>."},
-{"icon": "SilkIcons/link_edit.png", "html": "Love your fellow artists via the <a href='http://dalinksystem.deviantart.com/'>#dALinkSystem</a>."}
+{"icon": "SilkIcons/medal_gold_2.png", "html": "There's lots of love-worthy art to be found at <a href='http://today.deviantart.com/dds/'>Daily Deviations</a>."},
+{"icon": "SilkIcons/link_edit.png", "html": "You'll love promoting your fellow artists via the <a href='http://dalinksystem.deviantart.com/'>#dALinkSystem</a>."},
+{"icon": "Watch.png", "html": "Keep the love going with additions to your <a href='http://help.deviantart.com/26/'>deviantWATCH</a>."},
+{"icon": "News.png", "html": "Check out <a href='http://news.deviantart.com/browse/culture/art_features/'>Art Features</a>, where reporters show you the art they love."}
 ]
