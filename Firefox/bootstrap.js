459be4946d79ecd13663d079e4314f216025af97
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2012-12-23 21:14:46 -0800

    Oh, right. Copyright notices.

--- Firefox/bootstrap.js
+++ Firefox/bootstrap.js
@@ -1,3 +1,8 @@
+/*
+	This file is part of Deviant Love.
+	Copyright 2010-2012 Pikadude No. 1
+	Check core.js for the complete legal stuff.
+*/
 "use strict";
 Components.utils.import("resource://gre/modules/Services.jsm");
 var windowWatcher = Services.ww;
