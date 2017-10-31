af43c00ad65aef0ed4e7ffd2fb6532eca79c628d
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2017-10-31 15:50:01 -0700

    Split fillL10n to a new file for core.js's use

--- WebExtension/options/options.js
+++ WebExtension/options/options.js
@@ -265,14 +265,3 @@ function printSyncStatus() {
 			.appendTo(syncElem);
 	}
 }
-
-function fillL10n(parent) {
-	for (let elem of Array.from( parent.querySelectorAll("[data-l10n]") )) {
-		var message = apiAdapter.getL10nMsg( elem.dataset.l10n );
-		if (elem.dataset.l10nAttr) {
-			elem.setAttribute(elem.dataset.l10nAttr, message);
-		} else {
-			elem.textContent = message;
-		}
-	}
-}
