207acd6a5a91e409a9e60419cc9a04cb72e78fd6
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2019-02-03 03:39:15 -0800

    Implemented headers that squish horizontally to avoid line breaks

---
+++ WebExtension/report/squishToFit.module.js
@@ -0,0 +1,34 @@
+/*
+	This file is part of Deviant Love.
+	Copyright Pikadude No. 1
+	Check core.module.js for the complete legal stuff.
+*/
+var registry = new Set();
+export default function(node) {
+	node.style.whiteSpace = "nowrap";
+	node.style.transformOrigin = "left";
+	node.parentElement.style.overflowX = "hidden";
+	squish(node);
+	registry.add(node);
+	return {
+		destroy() {
+			registry.delete(node);
+		},
+	};
+}
+window.addEventListener("resize", () => {
+	for (let node of registry) {
+		squish(node);
+	}
+});
+function squish(node) {
+	var range = document.createRange();
+	range.selectNodeContents(node);
+	var squishTo = node.getBoundingClientRect().width;
+	var squishFrom = range.getBoundingClientRect().width;
+	if (squishTo >= squishFrom) {
+		node.style.transform = "none";
+	} else {
+		node.style.transform = `scaleX(${squishTo / squishFrom})`;
+	}
+}
