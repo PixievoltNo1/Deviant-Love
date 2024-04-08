27fa66da11c3e5e929f7ea553441c3b3c6f81f0a
Pixievolt No. 1 <pikadudeno1+git@gmail.com>
2024-04-08 01:33:39 -0700

    Semver-major dependency updates

--- WebExtension/l10nStore.esm.js
+++ WebExtension/l10nStore.esm.js
@@ -4,19 +4,19 @@
 	Check core.esm.js for the complete legal stuff.
 */
 import { getL10nMsg } from "./apiAdapter.esm.js";
-import { FluentBundle } from "@fluent/bundle";
+import { FluentBundle, FluentResource } from "@fluent/bundle";
 import { writable } from "svelte/store";
 var store = writable( () => "" );
 export { store as default };
 export async function init(langPref) {
 	var response = await fetch( getL10nMsg("fileFluent") );
-	var ftl = await response.text();
+	var ftl = new FluentResource( await response.text() );
 	var bundle = new FluentBundle('en-US');
-	var errors = bundle.addMessages(ftl);
+	var errors = bundle.addResource(ftl);
 	logAll(errors);
 	store.set( (msg, args) => {
 		var errors = [];
-		var text = bundle.format( bundle.getMessage(msg), args, errors );
+		var text = bundle.formatPattern( bundle.getMessage(msg).value, args, errors );
 		logAll(errors);
 		return text;
 	} );
